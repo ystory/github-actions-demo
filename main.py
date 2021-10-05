2c3aaed7ada59a926b7d13e9c43f666254ed5248
Tory Yongsul Kim <tory.k@krustuniverse.com>
2021-10-05 13:45:16 +0900

    Add boto

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import boto3
 from fastapi import FastAPI
 
 app = FastAPI()
-s3 = boto3.resource('s3')
+sts = boto3.client('sts')
 
 
 @app.get("/")
 async def root():
-    return {"buckets": ",".join(bucket.name for bucket in s3.buckets.all())}
+    return sts.get_caller_identity()
